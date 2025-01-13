34ada7552db6230c614faca71dc2f82248c05ffe
إسحاق <discord50557@gmail.com>
2025-01-13 21:22:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,16 @@ import {
   ModalBuilder,
   TextInputBuilder,
 } from "@discordjs/builders";
-
+import express from  "express";
 dotenv.config();
-
+const app = express();
+var listenir = app.listen(2000,()=>console.log(`listening at port: ${listenir.adress().port}`));
+app.listen(()=>{
+  console.log("ready to work 24h!");
+})
+app.get("/"(req,res)=>{
+  res.send("<body><center><h1>bot ready 24h</h1></center></body>")
+})
 // التحقق من المتغيرات
 const { TOKEN, CLIENT_ID, GUILD_ID, DB, adminRole } = process.env;
 if (!TOKEN || !CLIENT_ID || !GUILD_ID || !DB) {
